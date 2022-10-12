42d97048517c0dcd2ce0e2f7a7206c87f65005ab
sadnanrakib <sadnanrakib02@gmail.com>
2022-10-12 11:16:17 +0600

    error

--- src/components/ErrorPage/Error.jsx
+++ src/components/ErrorPage/Error.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Error = () => {
     return (
-        <div>
+        <div className='error'>
+            <img src="https://cdn.pixabay.com/photo/2017/02/12/21/29/false-2061132__340.png" alt="" />
            <h1>404</h1>
            <h4>Not Found</h4> 
         </div>
